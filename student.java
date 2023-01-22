b637b3f9d6840e8d7a60d61e1e0185ae586a312d
sivakumari20 <123285641+sivakumari20@users.noreply.github.com>
2023-01-22 10:26:49 +0530

    Create student.java

---
+++ student.java
@@ -0,0 +1,12 @@
+class Student
+{
+	int id;//data member (also instance variable)
+	String name; //data member (also instance variable)
+
+	public static void main(String args[])
+	{
+		Student s1=new Student();//creating an object of Student
+		System.out.println(s1.id);
+		System.out.println(s1.name);
+	}
+}
